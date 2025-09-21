948dc63411810563c658506d0a78dbb0af06b31e
dandgepratik2 <pratikdandge333@gmail.com>
2025-09-21 22:36:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
+function playSound(keyCode) {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${keyCode}"]`);
+  if (!audio) return;  
+
+  key.classList.add('playing');
+  audio.currentTime = 0;  
+  audio.play();
+}
+
 function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
 
-  function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
+const keys = Array.from(document.querySelectorAll('.key'));
+ 
+keys.forEach(key => {
+  key.addEventListener('click', function() {
+    const keyCode = this.getAttribute('data-key');
+    playSound(keyCode);
+  });
+  key.addEventListener('transitionend', removeTransition);
+});
 
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-  }
-
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+window.addEventListener('keydown', function(e) {
+  playSound(e.keyCode);
+});
